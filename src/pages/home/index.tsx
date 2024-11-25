6496ee09ef43e0f26b77df1cdd26bf810f9a7158
iam7kei <iam7kei@gmail.com>
2024-11-25 20:31:19 +0800

    added strikethrough

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,5 +1,4 @@
-import React, { useState, useEffect } from "react";
-
+import React, { useState } from "react";
 import { Todo } from "../../components/todo";
 import { TodoType } from "../../utils/constants";
 
@@ -23,20 +22,24 @@ export const Home = () => {
     setTodo("");
   };
 
+  const setIsChecked = (index: number) => {
+    const updatedTodos = [...todos]
+    updatedTodos[index].isChecked = true;
+    setTodos(updatedTodos);
+  }
+
   return (
-    <div className="w-[700px]">
-      {isTodoEmpty && (
-        <p className="text-red-500 font-bold">Please enter the TODO</p>
-      )}
-      <div className="bg-white w-full rounded-md px-5 py-2 mb-10 flex flex-row space-x-5">
+    <div className="w-full ">
+      {isTodoEmpty && <p className="font-bold">Please enter the TODO</p>}
+      <div className="bg-white w-full rounded-md px-5 py-2 mb-10 flex flex-row space-x-5 ">
         <input
           type="text"
-          className="bg-white border-0 focus:outline-none text-black w-3/4"
+          className="bg-white border-0 focus:outline-none text-black w-full"
           placeholder="Enter TODO here"
           value={todo}
           onChange={(e) => setTodo(e.target.value)}
         />
-        <div className="space-x-4 self-end  w-1/4 flex flex-row">
+        <div className="space-x-4 self-end flex flex-row">
           <button
             type="button"
             className="bg-green-400 outline-none focus:outline-none border-0"
@@ -53,15 +56,21 @@ export const Home = () => {
           </button>
         </div>
       </div>
-      <div className="overflow-hidden h-full w-full ">
+      <div className="overflow-clip h-full w-full ">
         {todos.length === 0 ? (
-          <div className="h-16 overflow-auto bg-white rounded-md items-center justify-center flex">
+          <div className="h-16 bg-white rounded-md items-center justify-center flex">
             <p className="text-black">There are no TODOs</p>
           </div>
         ) : (
-          <div className="max-h-[500px] overflow-auto bg-white rounded-md ">
-            {todos.map((item: TodoType) => (
-              <Todo todo={item.todo} isChecked={item.isChecked} />
+          <div className="max-h-[500px] w-full bg-white rounded-md">
+            {todos.map((item: TodoType, index) => (
+              <Todo
+                todo={item.todo}
+                isChecked={item.isChecked}
+                key={index}
+                index={index}
+                setIsChecked={setIsChecked}
+              />
             ))}
           </div>
         )}
